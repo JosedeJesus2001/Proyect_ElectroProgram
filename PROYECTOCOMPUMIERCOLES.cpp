35539ece38e8ff790b0a3092d91f2cba4641d787
JosedeJesus2001 <90534765+JosedeJesus2001@users.noreply.github.com>
2021-10-19 19:05:04 -0500

    Update PROYECTOCOMPUMIERCOLES.cpp

--- PROYECTOCOMPUMIERCOLES.cpp
+++ PROYECTOCOMPUMIERCOLES.cpp
@@ -5,24 +5,52 @@
  */
 #include <iostream>
 #include <array>
+#include <stdio.h>
+#include <string>
 using namespace std;
 int main() {
+    char k;
     cout << "Este programa te ayuda a obtener valores de resistencias, voltajes y corrientes" <<endl;
     cout<<"¿Deseas hacer un análisis de circuito o deseas sacar una resistencia equivalente?"<<endl;
-    cout<<"Si deseas hacer un análisis de circuito escribe 1 y si deseas obtener una resistencia equivalente escribe 2"<<endl;
-   int Opccion;
-    cin>>Opccion;
-    if (Opccion=2){ //Aquí en ves del if podemos utilizar un switch para utilizar texto en vez de números.
-        int NR; //Números de resistencias
-        cout<<"¿Cuantas resistencias tienes?"<<endl;
-        cin>>NR;
-        //Hay que hacer un arreglo que guarde aquí los valores de n cantidad de resistencias.
-       
+    cout<<"Si deseas hacer un análisis de circuito escribe c y si deseas obtener una resistencia equivalente escribe r"<<endl;
+    scanf("%c", &k);
+    if (k== 'r' ){
+        char h;
+        cout<<"¿Desear sumar resistencias en paralelo o en serie?"<<endl;
+        cout<<"Si deseas sumar resistencias en paralelo escribe la letra p y en el caso contrario escribe la letra s"<<endl;
+        cin>>h;
+        if (h=='p'){
+            int rs;
+            cout<<"Ingresa el número de reistencias a sumar"<<endl;
+            cin>>rs;
+            int contador1=1;
+            int almacenar1;
+            float ayuda=0;
+            float divisor1;
+            float divisor2;
+            while (contador1<rs+1){
+                cout<<"Ingrese el valor de su resistencia "<<contador1<<endl;
+                cin>>almacenar1;
+                contador1=contador1+1;
+                divisor1=(1/almacenar1);
+                divisor1=(divisor1)+(1/almacenar1);
+
+
+            }
+            cout<<divisor1<<endl;
+            float resultadoparcial;
+            resultadoparcial=(1/divisor1);
+            cout<<"El resultado de la suma de resistencias en paralelo de este ciclo es "<<resultadoparcial<<endl;
+            cout<<"¿Desea almacenar ese valor para utilizarlo más tarde?"<<endl;
 
         }
+
+                }
 
 
 
     return 0;
 
     }
+
+
